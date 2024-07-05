c0e17dde0fddabdc6d2daca5b9121a216019a88f
R. S. Doiel <rsdoiel@gmail>
2024-07-05 11:40:58 -0700

    Quick Save

--- dataset.ts
+++ dataset.ts
@@ -47,6 +47,12 @@ export class DatasetApiClient {
    * constructor for object
    * @param {number} port is the port number to use when request data from datasetd service
    * @param {sting} c_name is the collection name used to form API requests.
+   *
+   * @example
+   *
+   * ```
+   *    const ds = new DatasetApiClient(8485, "test.ds");
+   * ```
    */
   constructor(port: number, c_name: string) {
     this.port = port;
@@ -56,6 +62,14 @@ export class DatasetApiClient {
 
   /**
    * keys retrieves a list of keys expressed as JSON from the datasetd JSON API
+   *
+   * @example
+   *
+   * ```
+   *    const ds = new DatasetApiClient(8485, "test.ds");
+   *    const key_list_text = await ds.keys();
+   *    const keys = JSON.parse(key_list_text);
+   * ```
    */
   async keys(): Promise<Response> {
     return await fetch(`${this.dataset_api}/keys`, {
@@ -67,6 +81,23 @@ export class DatasetApiClient {
   /**
    * create retrieves creates a new object in the collection from a key and JSON encoded object.
    * It does this through the datasetd JSON API.
+   *
+   * @param {string} key
+   * @param {string} body
+   * @returns {Promise<Response>}
+   *
+   * @example
+   *
+   * ```
+   *    const ds = new DatasetApiClient(8485, "test.ds");
+   *    let obj = {
+   *       "one": 1,
+   *       "two": false,
+   *       "three": "one for all and all for one"
+   *    };
+   *    let key = "3musketeers";
+   *    let resp = await ds.create(key, JSON.stringify(obj));
+   * ```
    */
   async create(key: string, body: string): Promise<Response> {
     return fetch(`${this.dataset_api}/object/${key}`, {
@@ -78,6 +109,17 @@ export class DatasetApiClient {
 
   /**
    * read retrieves a single object expressed as JSON from the datasetd JSON API
+   *
+   * @param {string} key
+   * @returns {Promise<Response>}
+   *
+   * @example
+   *
+   * ```
+   *    const ds = new DatasetApiClient(8485, "test.ds");
+   *    let key = "3musketeers";
+   *    let resp = await ds.read(key);
+   * ```
    */
   async read(key: string): Promise<Response> {
     return fetch(`${this.dataset_api}/object/${key}`, {
@@ -89,6 +131,23 @@ export class DatasetApiClient {
   /**
    * update replaces an new object in the collection from a key and JSON encoded object.
    * It does this through the datasetd JSON API.
+   *
+   * @param {string} key
+   * @param {string} body
+   * @returns {Promise<Response>}
+   *
+   * @example
+   *
+   * ```
+   *    const ds = new DatasetApiClient(8485, "test.ds");
+   *    let obj = {
+   *       "one": 1,
+   *       "two": false,
+   *       "three": "one for all and all for one"
+   *    };
+   *    let key = "3musketeers";
+   *    let resp = await ds.update(key, JSON.stringify(obj));
+   * ```
    */
   async update(key: string, body: string): Promise<Response> {
     return fetch(`${this.dataset_api}/object/${key}`, {
@@ -101,6 +160,17 @@ export class DatasetApiClient {
   /**
    * delete removes an new object from the collection from given key.
    * It does this through the datasetd JSON API.
+   *
+   * @param {string} key
+   * @returns {Promise<Response>}
+   *
+   * @example
+   *
+   * ```
+   *    const ds = new DatasetApiClient(8485, "test.ds");
+   *    let key = "3musketeers";
+   *    let resp = await ds.delete(key);
+   * ```
    */
   async delete(key: string): Promise<Response> {
     return fetch(`${this.dataset_api}/object/${key}`, {
@@ -119,6 +189,17 @@ export class DatasetApiClient {
    * @param {string[]} fields, an ordered array of fields defined in the query
    * @param {string} body holds the JSON encoded key/value pairs for the query
    * @returns {Promise<Repsonse>}
+   *
+   * @example
+   *
+   * ```
+   *    const ds = new DatasetApiClient(8485, "test.ds");
+   *    // NOTE: the backup_object query doesn't take parameters
+   *    // so fields and body are empty
+   *    const query_name = "backup_objects";
+   *    let key = "3musketeers";
+   *    let resp = await ds.query(query_name, [], '');
+   * ```
    */
   async query(
     query_name: string,
@@ -154,8 +235,15 @@ export class Dataset {
 
   /**
    * constructor for object
+   *
    * @param {number} port is the port number to use when request data from datasetd service
    * @param {sting} c_name is the collection name used to form API requests.
+   *
+   * @example
+   *
+   * ```
+   *    const ds = new Dataset(8485, "test.ds");
+   * ```
    */
   constructor(port: number, c_name: string) {
     this._ds = new DatasetApiClient(port, c_name);
@@ -163,6 +251,13 @@ export class Dataset {
 
   /**
    * keys returns a list of all keys from a dataset collection
+   *
+   * @example
+   *
+   * ```
+   *    const ds = new Dataset(8485, "test.ds");
+   *    const keys = await ds.keys();
+   * ```
    */
   async keys(): Promise<string[]> {
     const resp = await this._ds.keys();
@@ -206,6 +301,14 @@ export class Dataset {
    *
    * @param {string} key, the key to the object you want to retrieve.
    * @returns {object} returns the object or undefined if it fails.
+   *
+   * @example
+   *
+   * ```
+   *    const ds = new Dataset(8485, "test.ds");
+   *    let key = "3musketeers";
+   *    let obj = await ds.read(key);
+   * ```
    */
   async read(key: string): Promise<object | undefined> {
     const resp = await this._ds.read(key);
@@ -224,6 +327,19 @@ export class Dataset {
    * @param {string} key
    * @param {object} obj is the replacement object for the key in the collection.
    * @returns {boolean} true if successful, false otherwise
+   *
+   * @example
+   *
+   * ```
+   *    const ds = new Dataset(8485, "test.ds");
+   *    let obj = {
+   *       "one": 1,
+   *       "two": false,
+   *       "three": "one for all and all for one"
+   *    };
+   *    let key = "3musketeers";
+   *    let resp = await ds.create(key, obj)l
+   * ```
    */
   async update(key: string, obj: object): Promise<boolean> {
     const text = JSON.stringify(obj);
@@ -245,6 +361,14 @@ export class Dataset {
    *
    * @param {string} key, the key to the object you want to remove.
    * @returns {boolean} true if successful, false otherwise
+   *
+   * @example
+   *
+   * ```
+   *    const ds = new Dataset(8485, "test.ds");
+   *    let key = "3musketeers";
+   *    let resp = await ds.delete(key);
+   * ```
    */
   async delete(key: string): Promise<boolean> {
     const resp = await this._ds.delete(key);
@@ -276,6 +400,16 @@ export class Dataset {
    * @returns {object}, usually a list or an empty list or object is returned if
    * there were no results or an error. Errors will be reported in the datasetd
    * log.
+   *
+   * @example
+   *
+   * ```
+   *    const ds = new DatasetApiClient(8485, "test.ds");
+   *    // NOTE: the backup_object query doesn't take parameters
+   *    // so fields and object are empty
+   *    const query_name = "backup_objects";
+   *    let resp = await ds.query(query_name, [], {});
+   * ```
    */
   async query(
     query_name: string,
--- docs/search_index.js
+++ docs/search_index.js
@@ -1,3 +1,3 @@
 (function () {
-  window.DENO_DOC_SEARCH_INDEX = {"nodes":[{"kind":["class"],"name":"Dataset","file":".","location":{"filename":"default","line":152,"col":0,"byteIndex":5360},"declarationKind":"export","deprecated":false},{"kind":["class"],"name":"DatasetApiClient","file":".","location":{"filename":"default","line":41,"col":0,"byteIndex":1883},"declarationKind":"export","deprecated":false}]};
+  window.DENO_DOC_SEARCH_INDEX = {"nodes":[{"kind":["class"],"name":"Dataset","file":".","location":{"filename":"default","line":233,"col":0,"byteIndex":7291},"declarationKind":"export","deprecated":false},{"kind":["class"],"name":"DatasetApiClient","file":".","location":{"filename":"default","line":41,"col":0,"byteIndex":1883},"declarationKind":"export","deprecated":false}]};
 })()
