66e50dd76e3becc6384a351fddc5c10ef003cabc
R. S. Doiel <rsdoiel@gmail>
2024-07-17 10:47:11 -0700

    switch from encodeURI(key) to encodeURIComponent(key)

--- docs/search_index.js
+++ docs/search_index.js
@@ -1,3 +1,3 @@
 (function () {
-  window.DENO_DOC_SEARCH_INDEX = {"nodes":[{"kind":["class"],"name":"Dataset","file":".","location":{"filename":"default","line":233,"col":0,"byteIndex":7335},"declarationKind":"export","deprecated":false},{"kind":["class"],"name":"DatasetApiClient","file":".","location":{"filename":"default","line":41,"col":0,"byteIndex":1883},"declarationKind":"export","deprecated":false}]};
+  window.DENO_DOC_SEARCH_INDEX = {"nodes":[{"kind":["class"],"name":"Dataset","file":".","location":{"filename":"default","line":233,"col":0,"byteIndex":7371},"declarationKind":"export","deprecated":false},{"kind":["class"],"name":"DatasetApiClient","file":".","location":{"filename":"default","line":41,"col":0,"byteIndex":1883},"declarationKind":"export","deprecated":false}]};
 })()
